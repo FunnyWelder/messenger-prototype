4eccf03fb9938ae97b5c621ab4eeafefa44d258b
FunnyWelder <slavakuzm@mail.ru>
2021-05-03 02:17:36 +1000

    Добавил twig

--- mess/ms2.php
+++ mess/ms2.php
@@ -1,52 +1,15 @@
-<html lang="ru">
-<head>
-    <style type="text/css">
-        html, body {
-            height: 100%;
-            width: 100%;
-        }
-
-        body {
-            background-color: #c0c0c0;
-            background-image: linear-gradient(335deg, #864735 23px, transparent 23px),
-            linear-gradient(155deg, #864735 23px, transparent 23px),
-            linear-gradient(335deg, #864735 23px, transparent 23px),
-            linear-gradient(155deg, #864735 23px, transparent 23px);
-            background-size: 58px 58px;
-            background-position: 0 2px, 4px 35px, 29px 31px, 34px 6px;
-            margin: 0;
-        }
-
-        .mess {
-            display: inline-block;
-            margin-right: 0;
-            margin-left: 15px;
-            float: left;
-            margin-top: 15px;
-            margin-bottom: 5px;
-            box-shadow: 0 0 4px black;
-            background: lightgray;
-            color: black;
-            width: 65%;
-            min-height: 7%;
-            height: auto;
-            padding-left: 15px;
-            padding-right: 15px;
-            text-align: left;
-            word-wrap: break-word;
-            font-size: 14pt;
-            border-radius: 20px;
-        }
-    </style>
-    <title></title>
-</head>
-<body>
 <?php //mess 2.1
+require_once 'vendor/autoload.php';
 $config = require_once 'src/config.php';
 $MYSQL_log = $config['MYSQL_LOG'];
 $MYSQL_user = $config['MYSQL_USER'];
 $MYSQL_bd = $config['MYSQL_BD'];
 $MYSQL_pass = $config['MYSQL_PASS'];
+
+$loader = new Twig\Loader\FilesystemLoader('templates');
+$twig = new Twig\Environment($loader, [
+    'cache' => 'compilation_cache'
+]);
 
 
 try {
@@ -57,12 +20,24 @@ try {
     require_once 'src/read_mess.php';
 
     add($db);
-    read($db);
+    $mass = read($db);
+
+    $name = $mass['name'];
+    $text = $mass['text'];
+    $time = $mass['time'];
+
+    echo $twig->render('index.twig');
+
+    for ($i=0; $i < count($name);$i++){
+        echo '<div class="mess"><b>Пользователь:</b> ';
+        echo $name[$i];
+        echo '<br/><b>Сообщение:</b> ';
+        echo $text[$i];
+        echo '<br/><b>Время:</b> ';
+        echo $time[$i];
+        echo '<br/></div>';
+    }
 
 } catch (PDOException $err) {
     print "Ошибка!: " . $err->getMessage() . "<br/>";
 }
-
-?>
-</body>
-</html>
--- mess/src/read_mess.php
+++ mess/src/read_mess.php
@@ -4,18 +4,23 @@ function read($db) {
     $query= $db->query('SELECT Ms.text, Ms.time, Pr.name FROM Mess as Ms, Profile as Pr 
             WHERE Ms.login = Pr.login ORDER BY Ms.time');
 
+    $name = [];
+    $text = [];
+    $time = [];
+    $i = 0;
+
     while($row = $query->fetch()) {
-        $name = $row['name'];
-        $text = $row['text'];
-        $time = $row['time'];
-        echo '<div class="mess"><b>Пользователь:</b> ';
-        echo $name;
-        echo '<br/><b>Сообщение:</b> ';
-        echo $text;
-        echo '<br/><b>Время:</b> ';
-        echo $time;
-        echo '<br/></div>';
+        $name[$i] = $row['name'];
+        $text[$i] = $row['text'];
+        $time[$i] = $row['time'];
+        $i++;
     }
 
-    return 'Сообщения выведены...';
+    $mass = [
+        'name' => $name,
+        'text' => $text,
+        'time' => $time
+    ];
+
+    return $mass;
 }
